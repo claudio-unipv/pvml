d95eba40774ed2e85a2db0cc61b36a782be9e96f
Claudio Cusano <claudio.cusano@unipv.it>
2020-01-04 13:29:33 +0100

    Docstrings for the MLP

--- pvml/mlp.py
+++ pvml/mlp.py
@@ -2,25 +2,57 @@ import numpy as np
 from multinomial_logistic import softmax
 
 
-# TODO:
-# - docstrings and comments
-
-
 class MLP:
-    """Multi-layer perceptron."""
+    """Multi-layer perceptron.
 
-    def __init__(self, units):
-        self.weights = []
-        self.biases = []
-        for a, b in zip(units[:-1], units[1:]):
-            w = np.random.randn(a, b)
-            w *= np.sqrt(2.0 / a)  # Kaiming initialization
-            self.weights.append(w)
-            self.biases.append(np.zeros(b))
+    A multi-layer perceptron for classification.
+
+    The activation function for the output layer is the softmax
+    operator, while hidden neurons use relu.
+
+    The loss function used during training is the cross entropy.
+
+    """
+
+    def __init__(self, neuron_counts):
+        """Create and initialiaze the MLP.
+
+        At least two layers must be specified (input and output).
+
+        Parameters
+        ----------
+        neuron_counts : list
+            number of neurons in the layers (first input, then hiddens,
+            then output).
+        """
+        # Initialize weights with the Kaiming technique.
+        self.weights = [np.random.randn(m, n) / np.sqrt(2.0 / n)
+                        for m, n in zip(neuron_counts[:-1], neuron_counts[1:])]
+        # Biases are zero-initialized.
+        self.biases = [np.zeros(m) for m in neuron_counts[1:]]
+        # Accumulators for the momentum terms
         self.update_w = [np.zeros_like(w) for w in self.weights]
         self.update_b = [np.zeros_like(b) for b in self.biases]
 
     def forward(self, X):
+        """Compute the activations of the neurons.
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+            input features (one row per feature vector).
+
+        Returns
+        -------
+        list
+            the list of activations of the neurons
+
+        The returned list contains arrays (one per layer) with the
+        activations of the layers.  The first element of the list is
+        the input X, followed by the activations of hidden layers and
+        trminated by the activations in the output layer.
+
+        """
         activations = [X]
         for W, b in zip(self.weights, self.biases):
             X = X @ W + b.T
@@ -32,6 +64,21 @@ class MLP:
         return activations
 
     def backward(self, Y, activations):
+        """Compute the derivatives of the loss wrt the activations.
+
+        Parameters
+        ----------
+        Y : ndarray, shape (m,)
+            target output (integer class labels).
+        activations : list
+            activations computed by the forward method.
+
+        Returns
+        -------
+        list
+            the list of computed derivatives, starting from output
+            activations and back to the first hidden layer.
+        """
         delta = activations[-1].copy()
         delta[np.arange(Y.shape[0]), Y] -= 1  # Subtract the one-hot vectors
         deltas = [delta]
@@ -41,11 +88,39 @@ class MLP:
             delta *= (X > 0).astype(int)  # derative of relu
         return deltas[::-1]
 
-    def backprop(self, X, Y, lr=1e-4, lambda_=1e-5, momentum=0.99):
+    def backpropagation(self, X, Y, lr=1e-4, lambda_=1e-5, momentum=0.99):
+        """Backpropagation algorithm.
+
+        Perform both the forward and the backward steps and update the
+        parameters.
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+            input features (one row per feature vector).
+        Y : ndarray, shape (m,)
+            target output (integer class labels).
+        lr : float
+            learning rate.
+        lambda_ : float
+            regularization coefficients.
+        momentum : float
+            momentum coefficient.
+
+        Returns
+        -------
+        float
+            the average loss obtained in the forward step..
+
+        """
+        # Forward pass
         activations = self.forward(X)
+        # Compute the average cross entropy loss
         probs = activations[-1][np.arange(Y.shape[0]), Y]
-        loss = -np.log(probs).mean()  # Cross entropy
+        loss = -np.log(probs).mean()
+        # Backward pass
         deltas = self.backward(Y, activations)
+        # Update the parameters
         for X, D, W, b, uw, ub in zip(activations, deltas,
                                       self.weights, self.biases,
                                       self.update_w, self.update_b):
@@ -60,11 +135,55 @@ class MLP:
         return loss
 
     def inference(self, X):
+        """Compute the predictions of the network.
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+            input features (one row per feature vector).
+
+        Returns
+        -------
+        ndarray, shape (m,)
+            predicted labels, in the range 0, ..., k - 1
+        ndarray, shape (m, k)
+            posterior probability estimates.
+
+        """
         probs = self.forward(X)[-1]
         labels = np.argmax(probs, 1)
         return labels, probs
 
-    def train(self, X, Y, lr=1e-4, lambda_=1e-5, momentum=0.99, steps=10000, batch=None):
+    def train(self, X, Y, lr=1e-4, lambda_=1e-5, momentum=0.99,
+              steps=10000, batch=None):
+        """Train the network.
+
+        Applu multiple steps of stochastic gradient descent.
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+            input features (one row per feature vector).
+        Y : ndarray, shape (m,)
+            target output (integer class labels).
+        lr : float
+            learning rate.
+        lambda_ : float
+            regularization coefficients.
+        momentum : float
+            momentum coefficient.
+        steps : int
+            training iterations.
+        batch : int or None
+            size of the minibatch used in each step.  When None all
+            the data is used in each step.
+
+        Returns
+        -------
+        list
+            the list of loss values obtained during training.
+
+        """
         m = X.shape[0]
         if batch is None:
             batch = X.shape[0]
@@ -75,17 +194,18 @@ class MLP:
             if i + batch > m:
                 i = 0
                 np.random.shuffle(indices)
-            loss = self.backprop(X[indices[i:i + batch], :],
-                                 Y[indices[i:i + batch]],
-                                 lr=lr,
-                                 lambda_=lambda_,
-                                 momentum=momentum)
+            loss = self.backpropagation(X[indices[i:i + batch], :],
+                                        Y[indices[i:i + batch]],
+                                        lr=lr,
+                                        lambda_=lambda_,
+                                        momentum=momentum)
             losses.append(loss)
             i += batch
         return losses
 
 
 def relu(x):
+    """ReLU activation function."""
     return np.maximum(x, 0)
 
 
@@ -94,8 +214,8 @@ if __name__ == "__main__":
 
     class Demo(demo.Demo):
         def train(self, X, Y):
-            units = [X.shape[1]] + self.args.hidden + [Y.max() + 1]
-            self.net = MLP(units)
+            neurons = [X.shape[1]] + self.args.hidden + [Y.max() + 1]
+            self.net = MLP(neurons)
             losses = self.net.train(X, Y, steps=self.args.steps,
                                     lr=self.args.lr,
                                     batch=self.args.batch)
