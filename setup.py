4c2cf6e879a6822a3c4672133f852724d0785075
Claudio Cusano <claudio.cusano@unipv.it>
2020-02-02 14:16:30 +0100

    Installation info and script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pvml-test",
+    version="0.0.1",
+    author="Claudio Cusano",
+    author_email="claudio.cusano@unipv.it",
+    description="A small and simple machine learning library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/claudio-unipv/pvml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy>=1.13.0",
+        "matplotlib>=2.1.0",
+    ],
+    python_requires='>=3.6',
+)
