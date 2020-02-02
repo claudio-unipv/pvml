265307c01cb1622d2825dba21b9ba00d9c2d5263
Claudio Cusano <claudio.cusano@unipv.it>
2020-02-02 14:24:25 +0100

    Updated the project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pvml-test",
+    name="pvml",
     version="0.0.1",
     author="Claudio Cusano",
     author_email="claudio.cusano@unipv.it",
